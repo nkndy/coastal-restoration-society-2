0ee740fef777fff93d085f3c87ac6db7980fbcc2
nkndy_ <nicholas.stuart.kennedy@gmail.com>
2018-05-29 12:31:00 -0700

    update stripe_payments

--- _includes/stripe_payment.js
+++ _includes/stripe_payment.js
@@ -1,43 +1,100 @@
+// Create a Stripe client.
+var stripe = Stripe('pk_test_sVOSwu1mjrLOs2C6m9Gjia8t');
 
-    var handler = StripeCheckout.configure({
-      key: 'pk_test_sVOSwu1mjrLOs2C6m9Gjia8t',
-      image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-      locale: 'auto',
-      token: function(token) {
-        $('.pay').prop("disabled", true);
-        $('.pay').text('Paying...')
-        $.ajax({
-            url: 'https://wt-b9fa931cbbfbac80477a7365ca8d3306-0.sandbox.auth0-extend.com/coastal-restoration-stripe',
-            type: 'POST',
-            data: {
-              stripeToken: token.id
-            }
-        }).then(function(stripeCustomer) {
-          console.log('success');
-        }).fail(function(e) {
-          $('.pay').text('Buy');
-          alert('There was an error processing the payment. Please try again.')
-        });
-      }
-    });
+// Create an instance of Elements.
+var elements = stripe.elements();
 
-    $(function() {
-      $('.pay').on('click', function(e) {
-        e.preventDefault();
-        console.log("hello");
-        handler.open({
-          name: 'Title',
-          description: 'My Subscription',
-          panelLabel: "Subscribe",
-          amount: 900, // 9 usd
-          email: 'nicholas.stuart.kennedy@gmail.com',
-          allowRememberMe: false
-        });
-      });
-    });
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '18px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
 
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
 
-    // close Checkout on page navigation
-    $(window).on('popstate', function() {
-      handler.close();
-    });
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+    //
+    // var handler = StripeCheckout.configure({
+    //   key: 'pk_test_sVOSwu1mjrLOs2C6m9Gjia8t',
+    //   image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+    //   locale: 'auto',
+    //   token: function(token) {
+    //     $('.pay').prop("disabled", true);
+    //     $('.pay').text('Paying...')
+    //     $.ajax({
+    //         url: 'https://wt-b9fa931cbbfbac80477a7365ca8d3306-0.sandbox.auth0-extend.com/coastal-restoration-stripe',
+    //         type: 'POST',
+    //         data: {
+    //           stripeToken: token.id
+    //         }
+    //     }).then(function(stripeCustomer) {
+    //       console.log('success');
+    //     }).fail(function(e) {
+    //       $('.pay').text('Buy');
+    //       alert('There was an error processing the payment. Please try again.')
+    //     });
+    //   }
+    // });
+    //
+    // $(function() {
+    //   $('.pay').on('click', function(e) {
+    //     e.preventDefault();
+    //     handler.open({
+    //       name: 'Title',
+    //       description: 'My Subscription',
+    //       panelLabel: "Subscribe",
+    //       amount: 900, // 9 usd
+    //       email: 'nicholas.stuart.kennedy@gmail.com',
+    //       allowRememberMe: false
+    //     });
+    //   });
+    // });
+    //
+    //
+    // // close Checkout on page navigation
+    // $(window).on('popstate', function() {
+    //   handler.close();
+    // });
--- index.md
+++ index.md
@@ -2,3 +2,51 @@
 title: Welcome To Coastal Restoration Society
 layout: home
 ---
+
+<style>
+/**
+ * The CSS shown here will not be introduced in the Quickstart guide, but shows
+ * how you can use CSS to style your Element's container.
+ */
+.StripeElement {
+  background-color: white;
+  height: 40px;
+  padding: 10px 12px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  box-shadow: 0 1px 3px 0 #e6ebf1;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+}
+
+.StripeElement--focus {
+  box-shadow: 0 1px 3px 0 #cfd7df;
+}
+
+.StripeElement--invalid {
+  border-color: #fa755a;
+}
+
+.StripeElement--webkit-autofill {
+  background-color: #fefde5 !important;
+}
+</style>
+
+
+<button class="pay">Pay</button>
+
+<form action="/charge" method="post" id="payment-form">
+  <div class="form-row">
+    <label for="card-element">
+      Credit or debit card
+    </label>
+    <div id="card-element">
+      <!-- A Stripe Element will be inserted here. -->
+    </div>
+
+    <!-- Used to display form errors. -->
+    <div id="card-errors" role="alert"></div>
+  </div>
+
+  <button>Submit Payment</button>
+</form>
